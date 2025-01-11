5c3dba1de751c69a95407cedff05431443a6cbca
Mark Wilkinson <mrk.wilkinson@protonmail.com>
2025-01-11 17:54:15 -0500

    Structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+
+extern crate Justice;
+use Justice::{C2Request, C2Response};
+use Justice::actions::{RequestActionType, ResponseActionType};
+use reqwest::Client;
+
+
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
